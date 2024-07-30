8bc451a8ceefca9eecfc6237ab4a4caa35a14be0
DiamondGotCat <124330624+DiamondGotCat@users.noreply.github.com>
2024-07-31 07:26:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # 0. Import Libraries
 import yfinance as yf
-import datetime
+from datetime import datetime
 import matplotlib.pyplot as plt
 from prophet import Prophet
 import pandas as pd
 
 # 1. Set information and Get Data
 id = input("ID of Stock: ")
-start = datetime.datetime(int(input("Start Year: ")), 1, 1)
-end = datetime.datetime(int(input("End Year: ")), 12, 30)
+start = datetime(int(input("Start Year: ")), int(input("Start Month: ")), int(input("Start Day(s): ")))
+end = datetime.now()
 df = yf.download(id, start=start, end=end)
 
 # Prepare data for Prophet
@@ -21,12 +21,12 @@ model = Prophet()
 model.fit(df)
 
 # 3. Make future dataframe and predict
-future = model.make_future_dataframe(periods=744)  # Forecasting for 744 days into the future
+future = model.make_future_dataframe(periods=1460)  # Forecasting for 4 years into the future
 forecast = model.predict(future)
 
 # 4. Visualize the forecast
 fig, ax = plt.subplots(figsize=(14, 7))
-ax.plot(df['ds'], df['y'], label='Historical Data', color='green')
+ax.plot(df['ds'], df['y'], label='Historical Data', color='blue')
 ax.plot(forecast['ds'], forecast['yhat'], label='Forecast', color='orange')
 ax.fill_between(forecast['ds'], forecast['yhat_lower'], forecast['yhat_upper'], color='orange', alpha=0.2)
 ax.set_title(f'{id} Stock Price Forecast')
